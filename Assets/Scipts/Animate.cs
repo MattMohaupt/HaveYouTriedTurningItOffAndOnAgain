aa4818f4ce5a6f46dfe7065ed6b18c29a6c7e93f
maryjosantos <msant22@uic.edu>
2025-04-09 00:30:52 -0500

    mecanim
    added animations for idle pose, picking up an object, and walking
    -- issues: walking animation stops after a few seconds

---
+++ Assets/Scipts/Animate.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Animate : MonoBehaviour {
+    private Animator anim;
+
+    void Start() {
+        // Get an instance of the Animator component attached to the character.
+        anim = GetComponent<Animator>();
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.W)) {
+            // Set the trigger value to True for the parameter Dance.
+            anim.SetTrigger("Walk");
+        }
+
+        if (Input.GetKeyDown(KeyCode.E)) {
+            // Set the trigger value to True for the parameter Dance.
+            anim.SetTrigger("PickUp");
+        }
+    }
+}
